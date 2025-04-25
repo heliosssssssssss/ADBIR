c56e68d2316845936abdb5137614b8c3ec50e7de
RJTHEGOAT13 <ranbirsinghjhass@gmail.com>
2025-04-25 21:43:50 +0100

    checkout to version2

---
+++ new-version/inputstream.py
@@ -0,0 +1,37 @@
+class inputStream(): 
+        
+    def __init__(self, _input):
+        self._input = _input
+        
+        self.Position = 0
+        self.Line = 1
+        self.Column = 0
+
+
+
+    def move(self): # looks at the next character, and incrememts position 
+        ch = self._input[self.Position]
+        self.Position += 1
+
+        if (ch == '\n'):
+            self.Line += 1
+            self.Column = 0
+        else: 
+            self.Column += 1
+        return ch
+        
+    def future(self): # looks at the next character without incrementing
+        if self.Position >= len(self._input):
+            return ""
+        return self._input[self.Position]
+        
+    def eol(self): # checks if the end of input has been reached
+        return self.future() == "" 
+        
+    def context(self, msg): # throws an error with a message, line and column number
+        raise Exception(f"{msg} ({self.Line}:{self.Column})")
+
+
+test = inputStream("hello world\n sigmas")
+while not test.eol():
+    print(f"CHAR: {test.move()} @ LINE : {test.Line}")
